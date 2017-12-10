d59eaa78a5a1b019f231163f5a4cba81951aefe9
Tom Henderson <tomhenderson@mac.com>
2017-12-11 08:23:50 +1300

    Move WIP to branches

--- cat-button/vars.tf
+++
@@ -1,8 +0,0 @@
-variable "phone_numbers" {
-  description = "Phone numbers to subscribe"
-  default     = []
-}
-
-variable "button_serial" {
-  description = "Serial number of IoT buttons to trigger lambda"
-}
--- slack-notifier/vars.tf
+++
@@ -1,8 +0,0 @@
-variable "slack_team_name" {
-  description = "Name of the slack team"
-}
-
-variable "slack_api_token_parameter" {
-  description = "Parameter store item containing the slack API token"
-  default     = "slack_api_token"
-}
