5986140695e4e5a515f7d764b9963e218b51ad99
Tom Henderson <tomhenderson@mac.com>
2017-12-11 08:25:44 +1300

    WIP :wrench:

---
+++ cat-button/main.tf
@@ -0,0 +1,34 @@
+resource "aws_sns_topic" "cats" {
+  name = "cat-pictures"
+}
+
+resource "aws_sns_topic_subscription" "subscription" {
+  count     = "${length(var.phone_numbers)}"
+  topic_arn = "${aws_sns_topic.cats.arn}"
+  protocol  = "sms"
+  endpoint  = "${element(var.phone_numbers, count.index)}"
+}
+
+module "lambda" {
+  source      = "../lambda"
+  name        = "cat_pictures_lambda"
+  description = "Put cat photo urls onto cat-pictures SNS topic."
+  source_code = "${path.module}/lambda.zip"
+  module_name = "lambda"
+
+  environment_variables = {
+    sns_topic = "${aws_sns_topic.cats.arn}"
+  }
+}
+
+resource "aws_lambda_permission" "allow_execution_from_sns" {
+  statement_id   = "AllowExecutionFromIoT"
+  action         = "lambda:InvokeFunction"
+  function_name  = "${module.lambda.function_name}"
+  principal      = "iot.amazonaws.com"
+  source_account = "${local.account_id}"
+  source_arn     = "arn:aws:iot:${local.region}:${local.account_id}:rule/iotbutton_${var.button_serial}"
+}
+
+# Still need to manually add the IoT Action on the button because terraform doesn't do it.
+
---
+++ cat-button/vars.tf
@@ -0,0 +1,8 @@
+variable "phone_numbers" {
+  description = "Phone numbers to subscribe"
+  default     = []
+}
+
+variable "button_serial" {
+  description = "Serial number of IoT buttons to trigger lambda"
+}
