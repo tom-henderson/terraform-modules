253d67d3815a81e608729419f272f26af0ec934a
Tom Henderson <tomhenderson@mac.com>
2017-12-10 17:28:52 +1300

    Lambda permission

--- cat-button/main.tf
+++ cat-button/main.tf
@@ -20,3 +20,23 @@ module "lambda" {
     sns_topic = "${aws_sns_topic.cats.arn}"
   }
 }
+
+resource "aws_lambda_permission" "allow_execution_from_sns" {
+  statement_id  = "AllowExecutionFromIoT"
+  principal     = "iot.amazonaws.com"
+  action        = "lambda:InvokeFunction"
+  resource      = "${module.lambda.arn}"
+  function_name = "${module.lambda.function_name}"
+
+  condition = {
+    test     = "StringEquals"
+    variable = "AWS:SourceAccount"
+    values   = ["${local.account_id}"]
+  }
+
+  condition = {
+    test     = "ArnLike"
+    variable = "AWS:SourceArn"
+    values   = ["arn:aws:iot:${local.region}:${local.account_id}:rule/iotbutton_${var.button_serial}"]
+  }
+}
