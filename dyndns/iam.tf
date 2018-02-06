9d03ba26593638d304964c9ccd090d398f7c643a
Tom Henderson <tomhenderson@mac.com>
2018-02-07 08:23:53 +1300

    Add instance name to policy

--- dyndns/iam.tf
+++ dyndns/iam.tf
@@ -1,17 +1,17 @@
-data "aws_iam_policy_document" "allow_update_route53_records" {
+data "aws_iam_policy_document" "allow_route53_update" {
   statement {
     actions   = ["route53:ChangeResourceRecordSets"]
     resources = ["arn:aws:route53:::hostedzone/${var.zone_id}"]
   }
 }
 
-resource "aws_iam_policy" "allow_update_route53_records" {
-  name   = "allow-update-route53-records"
+resource "aws_iam_policy" "allow_route53_update" {
+  name   = "${local.module_name}_allow_route53_update"
   path   = "/"
-  policy = "${data.aws_iam_policy_document.allow_update_route53_records.json}"
+  policy = "${data.aws_iam_policy_document.allow_route53_update.json}"
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_allow_update_route53_records" {
+resource "aws_iam_role_policy_attachment" "allow_route53_update" {
   role       = "${module.lambda.iam_role_name}"
-  policy_arn = "${aws_iam_policy.allow_update_route53_records.arn}"
+  policy_arn = "${aws_iam_policy.allow_route53_update.arn}"
 }
