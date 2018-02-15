58002079d58150fb928a63ef119cf9104780771b
Tom Henderson <tomhenderson@mac.com>
2018-02-16 08:37:39 +1300

    Slash command module

---
+++ slash-command/api.tf
@@ -0,0 +1,35 @@
+resource "aws_api_gateway_rest_api" "api" {
+  name = "${var.name}_api"
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  path_part   = "${var.name}"
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
+  resource_id             = "${aws_api_gateway_resource.resource.id}"
+  http_method             = "${aws_api_gateway_method.method.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${module.lambda.arn}/invocations"
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  stage_name  = "production"
+  depends_on  = ["aws_api_gateway_integration.integration"]
+}
+
+output "slash_command_invocation_url" {
+  value = "${aws_api_gateway_deployment.deployment.invoke_url}${aws_api_gateway_resource.resource.path}"
+}
---
+++ slash-command/vars.tf
@@ -0,0 +1,35 @@
+variable "name" {}
+variable "description" {}
+variable "source_code" {}
+
+variable "runtime" {
+  default = "python2.7"
+}
+
+variable "module_name" {
+  default = "lambda"
+}
+
+variable "handler" {
+  default = "lambda_handler"
+}
+
+variable "memory_size" {
+  default = "128"
+}
+
+variable "timeout" {
+  default = "20"
+}
+
+variable "log_retention_in_days" {
+  default = 1
+}
+
+variable "environment_variables" {
+  default = {}
+}
+
+variable "ssm_parameters" {
+  default = []
+}
