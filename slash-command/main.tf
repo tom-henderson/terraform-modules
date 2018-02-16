5fc1e2103c329fceb590d9d8fd8e03de200d5d24
Tom Henderson <tomhenderson@mac.com>
2018-02-17 11:12:56 +1300

    Update execution arn

--- slash-command/main.tf
+++ slash-command/main.tf
@@ -19,7 +19,7 @@ resource "aws_lambda_permission" "allow_execution" {
   principal     = "apigateway.amazonaws.com"
 
   # http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "arn:aws:execute-api:${local.region}:${local.account_id}:${aws_api_gateway_rest_api.api.id}/*${aws_api_gateway_resource.resource.path}"
+  source_arn = "arn:aws:execute-api:${local.region}:${local.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
 }
 
 output "lambda_arn" {
